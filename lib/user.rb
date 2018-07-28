972332a4ad75a3c71ddc00c4680c1fbb8935fede
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 20:09:30 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
-class User 
+class User
 
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-  end 
+  end
 
 end 
